03b29b8fdf30e115d907e5d6b7dd9046ddbf3123
huiwengoh <45724323+huiwengoh@users.noreply.github.com>
2022-08-23 16:39:53 -0400

    add benchmark files

---
+++ README.md
@@ -0,0 +1,50 @@
+# Multiannotator Benchmark
+Benchmarking Cleanlab extension for multiannotator data labeling.
+
+Our goal is to come up with algorithms that will:
+1. Provide better consensus labels
+2. Compute consensus quality scores 
+3. Compute annotator quality scores
+
+## Dataset Variants
+Our primary dataset for the benchmark is the [CIFAR-10H](https://github.com/jcpeterson/cifar-10h) dataset, which contains multiple annotator labels for the [CIFAR-10](https://www.cs.toronto.edu/~kriz/cifar.html) test dataset.
+
+We made additional variations of the CIFAR-10H dataset for our benchmarks, details of which are explained below:
+
+#### 1. `complete`
+This is the full, unaltered CIFAR-10H dataset, which contains 2571 annotators who each annotator 200 examples. Each example in this dataset has approximately 50 labels.
+
+#### 2. `uniform_1_5`
+To increase sparsity in the dataset, we subsetted the CIFAR10-H dataset such that each example now has between 1 to 5 labels (number of labels per example are uniformly distributed). After randomly dropping annotators and labels, our dataset contains 421 annotators.
+
+#### 3. `worst_annotators`
+This variation of the dataset selects the annotators that have the lowest accuracies when comparing their labels to the true labels. The sparsity and number of annotations per examples in this dataset is similar to `uniform_1_5` however the annotators are more likely to make label errors. This dataset contains 511 annotators.
+
+## Evaluation Metrics
+
+To measure the three goals stated above, we used the following metrics:
+
+1. To evaluate **how well our methods can provide consensus labels**, we measure the accuracy of the consensus labels against the ground truth CIFAR-10 labels
+
+2. To evaluate **how well the consensus quality scores estimate the quality of the consensus labels**, we measure the AUROC and AUPRC of whether or not the consensus label is accurate with respect to the ground truth label
+
+3. To evaluate **how well the annotator quality scores estimate the quality of each annotator**, we measure the spearman correlation of the annotator quality score and the accuracy of each annotator's labels with respect to the ground truth label
+
+
+
+## Methods Benchmarked
+
+Here is a brief description of all the methods we used in this benchmark.
+
+| Method Name | Description |
+| --- | ----------- |
+| `crowdlab` | Weighted ensemble model that treats each annotator and the model as a predictor, weights for the ensemble are determined using the annotator quality scores | 
+| `agreement` | Percentage of labels that agree with the consensus label |
+| `label_quality_score` | cleanlab's `label_quality_score` of the consensus labels |
+| `active_label_cleaning` | Ranks labels by noise, [link to paper](https://www.nature.com/articles/s41467-022-28818-3.pdf?origin=ppub) for more information |
+| `no_perannotator_weights` | Similar idea to `crowdlab`, however all annotators are aggregated then weighed against the model |
+| `empirical_bayes` | Treats the model prediction as prior distribution for true label and annotator labels as observations for a posterior distribution |
+| `dawid_skene` |  An algorithm for crowdsourced vote aggregation based off the principle of Expectation-Maximization, [link to paper](https://www.jstor.org/stable/2346806?origin=crossref) for more information |
+| `dawid_skene_with_model` | Adding the model predictions as an additional annotator, then running the Dawid-Skene algorithm above |
+| `glad` | Uses probabilistic approaches to infer the label, the expertise of each annotator and the difficulty of each example, [link to paper](https://proceedings.neurips.cc/paper/2009/file/f899139df5e1059396431415e770c6dd-Paper.pdf) for more information |
+| `glad_with_model` |  Adding the model predictions as an additional annotator, then running the GLAD algorithm above |
---
+++ benchmark_functions.py
@@ -0,0 +1,173 @@
+import sys
+import os
+from pathlib import Path
+
+sys.path.insert(0, "../")
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import (
+    roc_auc_score,
+    precision_recall_curve,
+    roc_curve,
+    precision_recall_fscore_support,
+    auc,
+)
+from matplotlib import pyplot as plt
+from scipy import stats
+
+# cleanlab imports
+from cleanlab.multiannotator import get_label_quality_multiannotator
+
+# local imports
+from utils.eval_metrics import lift_at_k
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+path = os.getcwd()
+fig_size_small = (5, 8)
+fig_size_big = (10, 7)
+
+
+def get_annotator_accuracy(annotator_labels, true_labels):
+    annotator_accuracy = pd.DataFrame(annotator_labels).apply(
+        lambda s: np.mean(s[pd.notna(s)] == true_labels[pd.notna(s)])
+    )
+    df_describe = pd.DataFrame(annotator_accuracy, columns=["score"])
+    return df_describe
+
+
+def get_consensus_label_accuracy(consensus_labels, true_labels):
+    consensus_labels_accuracy = np.mean(true_labels == consensus_labels)
+    p, r, f1, _ = precision_recall_fscore_support(true_labels, consensus_labels)
+    results_df = pd.DataFrame(zip(p, r, f1), columns=["precision", "recall", "f1"])
+    return consensus_labels_accuracy, results_df
+
+
+def get_spearman_correlation(x, y):
+    num_nans_x = np.sum(np.isnan(x))
+    num_nans_y = np.sum(np.isnan(y))
+
+    if num_nans_x > 0:
+        x = np.nan_to_num(x)
+    if num_nans_y > 0:
+        y = np.nan_to_num(y)
+
+    return stats.spearmanr(x, y)
+
+
+def benchmark_results(
+    c10h_labels,
+    c10h_true_labels,
+    pred_probs,
+    methods,
+    model_name,
+    dataset_name,
+    add_model=False,
+):
+    plt.rcParams["figure.figsize"] = fig_size_big
+    results_list = []
+
+    c10h_labels = pd.DataFrame(c10h_labels)
+
+    for consensus_method, quality_method in methods:
+        (
+            label_quality_multiannotator,
+            multiannotator_stats,
+        ) = get_label_quality_multiannotator(
+            c10h_labels,
+            pred_probs,
+            consensus_method=consensus_method,
+            quality_method=quality_method,
+            return_annotator_stats=True,
+            verbose=False,
+        )
+
+        # create boolean mask of label errors
+        labels = label_quality_multiannotator["consensus_label"]
+        label_errors_target = (
+            labels != c10h_true_labels
+        )  # labels can change to annotator labels!!
+
+        # compute scores
+        label_quality_scores = label_quality_multiannotator["quality_of_consensus"]
+        ranked_quality = label_quality_multiannotator["ranked_quality"]
+
+        consensus_labels = label_quality_multiannotator["consensus_label"]
+        consensus_labels_accuracy, results_df = get_consensus_label_accuracy(
+            consensus_labels, c10h_true_labels
+        )
+
+        quality_score = multiannotator_stats.sort_index()["overall_quality"].values
+
+        annotator_accuracy_df = get_annotator_accuracy(c10h_labels, c10h_true_labels)
+        accuracy = annotator_accuracy_df["score"].values
+
+        speaman_corr = get_spearman_correlation(quality_score, accuracy)
+
+        # compute precision-recall curve using label quality scores
+        precision, recall, thresholds = precision_recall_curve(
+            label_errors_target, -ranked_quality
+        )
+
+        # compute au-roc curve using label quality scores
+        fpr, tpr, thresholds = roc_curve(label_errors_target, -ranked_quality)
+
+        # compute accuracy of detecting label errors
+        auroc = roc_auc_score(label_errors_target, -ranked_quality)
+        auprc = auc(recall, precision)
+
+        lift_at_k_dict = {}
+        for k in [10, 50, 100, 300, 500, 1000]:
+            lift_at_k_dict[f"lift_at_{k}"] = lift_at_k(
+                label_errors_target, -ranked_quality, k=k
+            )
+
+        if add_model == True:
+            consensus_method += "_with_model"
+            quality_method += "_with_model"
+
+        # save results
+        results = {
+            "dataset": dataset_name,
+            "model": model_name,
+            "consensus_method": consensus_method,
+            "quality_method": quality_method,
+            "consensus_quality_auroc": auroc,
+            "consensus_quality_auprc": auprc,
+            "consensus_labels_accuracy": consensus_labels_accuracy,
+            "annotator_quality_spearman_corr": speaman_corr.correlation,
+        }
+
+        results.update(lift_at_k_dict)
+
+        # save results
+        results_list.append(results)
+
+        # plot prc
+        plt.subplot(1, 2, 1)
+        plt.plot(recall, precision, label=f"{quality_method}")
+        plt.xlabel("Recall", fontsize=14)
+        plt.ylabel("Precision", fontsize=14)
+        plt.title(
+            f"Precision-Recall Curve \n Model: {model_name} \n Dataset: {dataset_name}",
+            fontsize=14,
+            fontweight="bold",
+        )
+        plt.legend()
+
+        # plot roc
+        plt.subplot(1, 2, 2)
+        plt.plot(fpr, tpr, label=f"{quality_method}")
+        plt.xlabel("False Positive Rate", fontsize=14)
+        plt.ylabel("True Positive Rate", fontsize=14)
+        plt.title(
+            f"AU ROC Curve \n Model: {model_name} \n Dataset: {dataset_name}",
+            fontsize=14,
+            fontweight="bold",
+        )
+        plt.legend()
+
+    return results_list
